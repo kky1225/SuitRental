907ab1e626316664274190a484cfddac0c66538b
Seo <Seo@192.168.219.101>
2021-09-14 19:14:56 +0900

    회원리스트

--- suitRental2/src/main/java/kr/member/action/ManagerPageAction.java
+++ suitRental2/src/main/java/kr/member/action/ManagerPageAction.java
@@ -1,10 +1,15 @@
 package kr.member.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
+import kr.member.dao.MemberDAO;
+import kr.member.vo.MemberVO;
+import kr.util.PagingUtil;
 
 public class ManagerPageAction implements Action{
 
@@ -16,6 +21,36 @@ public class ManagerPageAction implements Action{
 		if(user_num == null) {	
 			return "redirect:/member/loginForm.do";
 		}
+		
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
+		if(user_auth < 3) {	// 관리자로 로그인하지 않은 경우
+			return "/WEB-INF/views/common/notice.jsp";
+		}
+		
+		// 관리자로 로그인 한 경우
+		String pageNum = request.getParameter("pageNum");
+		if(pageNum == null) pageNum="1";
+		
+		String keyfield = request.getParameter("keyfield");
+		String keyword = request.getParameter("keyword");
+		if(keyfield == null) keyfield=null;
+		if(keyword == null) keyword=null;
+
+		MemberDAO dao = MemberDAO.getInstance();
+		int count = dao.getMemberCountByAdmin(keyfield, keyword);
+		
+		// 페이지 처리
+		// keyfield,keyword,currentPage,count,rowCount,pageCount,url
+		PagingUtil page = new PagingUtil(keyfield, keyword, Integer.parseInt(pageNum), count, 20, 10, "memberList.do");
+		
+		List<MemberVO> list = null;
+		if(count > 0) {
+			list = dao.getListMemberByAdmin(page.getStartCount(), page.getEndCount(), keyfield, keyword);
+		}
+		
+		request.setAttribute("count", count);
+		request.setAttribute("list", list);
+		request.setAttribute("pagingHtml", page.getPagingHtml());
 		
 		return "/WEB-INF/views/member/managerPage.jsp";
 	}
